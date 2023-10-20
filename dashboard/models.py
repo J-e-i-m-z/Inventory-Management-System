e2308e83290de1e43c37f413a444761be99aeefd
James Nyamburi <nyamburijames@gmail.com>
2023-10-20 23:55:13 +0300

    first model

--- dashboard/models.py
+++ dashboard/models.py
@@ -11,3 +11,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
     category = models.CharField(max_length=20, choices = CATEGORY, null=True)
     quantity = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.name}-{self.quantity}'
+
